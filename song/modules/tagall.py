69ea61d602e8ae575bb1c9f519486057eb3b6970
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-05-25 21:52:22 +0400

    Create tagall.py

---
+++ song/modules/tagall.py
@@ -0,0 +1,19 @@
+from pyrogram import filters
+
+from song import app, LOGGER
+
+
+@app.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
+async def tagall(client, message):
+    await message.reply("`Tag Basladi!...`")
+    sh = get_text(message)
+    if not sh:
+        sh = "Salam!"
+    mentions = ""
+    async for member in client.iter_chat_members(message.chat.id):
+        mentions += member.user.mention + " "
+    n = 4096
+    kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
+    for i in kk:
+        j = f"<b>{sh}</b> \n{i}"
+        await client.send_message(message.chat.id, j, parse_mode="html")
