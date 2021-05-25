2ef373c0e77e590cb3226ea05cd9d7c7bf9ee34f
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-05-25 21:57:46 +0400

    Update mrdarkprince.py

--- song/mrdarkprince.py
+++ song/mrdarkprince.py
@@ -18,3 +18,15 @@ def get_arg(message):
     if " ".join(split[1:]).strip() == "":
         return ""
     return " ".join(split[1:])
+
+
+def get_text(message: Message) -> [None, str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
