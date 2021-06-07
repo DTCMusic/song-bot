d33c2d4fd264942996be882f65015ecb94e06eb4
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-06-07 14:13:02 +0400

    Update song.py

--- song/modules/song.py
+++ song/modules/song.py
@@ -68,7 +68,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**🎵 <i>{title}</i>**", {parse_mode='HTML'}
+        rep = f"🎵 <i>{title}</i>"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -77,7 +77,7 @@ def song(client, message):
             audio_file,
             caption=rep,
             thumb=thumb_name,
-            parse_mode="md",
+            parse_mode="HTML",
             title=title,
             duration=dur,
         )
