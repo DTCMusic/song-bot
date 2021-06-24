6026e2b0d9802807fc027137ef447b3f6743beaa
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-06-24 19:10:04 +0400

    Update tagall.py

--- song/modules/tagall.py
+++ song/modules/tagall.py
@@ -11,18 +11,7 @@ import asyncio
 from telethon.tl.types import ChannelParticipantsAdmins
 import telethon
 from telethon import events
-# from song.sql.chat_sql import 
 
-@app.on_message(filters.command("tag") & ~filters.channel)
-async def _(event):
-    if event.fwd_from:
-        return 
-    mentions = event.pattern_match.group(1)
-    chat = await event.get_input_chat()
-    async for x in bot.iter_participants(chat, 100):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    await event.reply(mentions)
-    await event.delete()
 
 @app.on_message(filters.command("all") & ~filters.channel)
 async def tagall(client, message):
@@ -33,25 +22,9 @@ async def tagall(client, message):
     mentions = ""
     async for member in client.iter_chat_members(message.chat.id):
         mentions += member.user.mention + " "
-    n = 4096
+    n = 6
     kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
     for i in kk:
-        j = f"<b>{sh}</b>, {i},"
+        j = f"<b>{sh}</b>\n{i}"
         await client.send_message(message.chat.id, j, parse_mode="html")
-    
-    
-@app.on_message(filters.command("admins") & ~filters.channel)
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "**Admins in this chat:** "
-    chat = await event.get_input_chat()
-    async for x in bot.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
+   
