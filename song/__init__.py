aa2149aa7917f2eac5a8ee9736615042df93d7bd
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-06-24 18:56:57 +0400

    Update __init__.py

--- song/__init__.py
+++ song/__init__.py
@@ -9,4 +9,16 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
+def get_text(message: Message) -> [None, str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
 app = Client("song", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
