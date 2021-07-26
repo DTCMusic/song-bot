f0753fb2067db1973e1c7280dc0fdf2d9cd716db
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-07-26 11:41:36 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+START_MSG = os.getenv("START_MSG")
 DATABASE_URL = os.getenv("DATABASE_URL")
 DURATION_LIMIT = int(os.getenv("DURATION_LIMIT", "7"))
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
