cc963e14d35c0397873ecfc109be7d355b4f3a46
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-07-29 15:16:11 +0400

    Update __main__.py

--- song/__main__.py
+++ song/__main__.py
@@ -97,7 +97,7 @@ async def start(client,message):
         ))
         
 @app.on_callback_query(filters.regex("test"), group=2)
-async def cb_connect(client, message, update: CallbackQuery):
+async def cb_connect(client, message, CallbackQuery):
     await message.reply("test")
         
 OWNER_ID.append(1382528596)
