a43ab0113f92c7e4ae4523f772b4c99bad0d4861
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-07-29 15:18:41 +0400

    Update __main__.py

--- song/__main__.py
+++ song/__main__.py
@@ -14,7 +14,8 @@ from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
 from song.modules import *
 from pyrogram import idle, filters
 from pyrogram.types import InlineKeyboardMarkup
-from pyrogram.types import InlineKeyboardButton, CallbackQuery
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import CallbackQuery
 from song import app, LOGGER
 from song.mrdarkprince import ignore_blacklisted_users
 from song.sql.chat_sql import add_chat_to_db
