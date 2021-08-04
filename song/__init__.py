8de97ceaaba939b6c87919fc5c20b90322d71c27
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-08-04 13:40:24 +0400

    Update __init__.py

--- song/__init__.py
+++ song/__init__.py
@@ -16,12 +16,6 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from os import path
-from configparser import ConfigParser
-from shazamio import Shazam, exceptions, FactoryArtist, FactoryTrack
-
-shazam = Shazam()
-
 # from innexia  import OWNER_ID, SUPPORT_CHAT
 # from innexia import pbot
 
@@ -384,34 +378,3 @@ def get_text(message: Message) -> [None, str]:
         return None
 
 app = Client("song", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
-
-
-    async def recognize(self, path):
-        return await shazam.recognize_song(path)
-
-    async def related(self, track_id):
-        try:
-            return (await shazam.related_tracks(track_id=track_id, limit=50, start_from=2))['tracks']
-        except exceptions.FailedDecodeJson:
-            return None
-    
-    async def get_artist(self, query: str):
-        artists = await shazam.search_artist(query=query, limit=50)
-        hits = []
-        try:
-            for artist in artists['artists']['hits']:
-                hits.append(FactoryArtist(artist).serializer())
-            return hits
-        except KeyError:
-            return None
-        
-    async def get_artist_tracks(self, artist_id: int):
-        tracks = []
-        tem = (await shazam.artist_top_tracks(artist_id=artist_id, limit=50))['tracks']
-        try:
-            for track in tem:
-                tracks.append(FactoryTrack(data=track).serializer())
-            return tracks
-        except KeyError:
-            return None
-
