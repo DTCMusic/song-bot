c4cec460d5281bf21a3e990394dc9acd6ba251e3
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-08-04 06:45:21 +0400

    Update __init__.py

--- song/__init__.py
+++ song/__init__.py
@@ -378,3 +378,34 @@ def get_text(message: Message) -> [None, str]:
         return None
 
 app = Client("song", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
+
+
+    async def recognize(self, path):
+        return await shazam.recognize_song(path)
+
+    async def related(self, track_id):
+        try:
+            return (await shazam.related_tracks(track_id=track_id, limit=50, start_from=2))['tracks']
+        except exceptions.FailedDecodeJson:
+            return None
+    
+    async def get_artist(self, query: str):
+        artists = await shazam.search_artist(query=query, limit=50)
+        hits = []
+        try:
+            for artist in artists['artists']['hits']:
+                hits.append(FactoryArtist(artist).serializer())
+            return hits
+        except KeyError:
+            return None
+        
+    async def get_artist_tracks(self, artist_id: int):
+        tracks = []
+        tem = (await shazam.artist_top_tracks(artist_id=artist_id, limit=50))['tracks']
+        try:
+            for track in tem:
+                tracks.append(FactoryTrack(data=track).serializer())
+            return tracks
+        except KeyError:
+            return None
+
