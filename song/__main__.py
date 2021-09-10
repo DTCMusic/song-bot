4c6181bec3109689761fe1273f17c43e8be3bc97
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-09-10 20:06:58 +0400

    Update __main__.py

--- song/__main__.py
+++ song/__main__.py
@@ -37,11 +37,6 @@ async def start(client, message):
                     InlineKeyboardButton(
                          text=f"{BTN_NAME}", url=f"{BTN_URL}" )
            
-                ],
-                [
-                    InlineKeyboardButton(
-                        text="🖥 Yeniləmə kanalı", url=f"https://t.me/{CLON_BOT}"
-                    )
                 ]
             ]
         )
