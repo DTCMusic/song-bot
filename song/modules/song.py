defcb20e7192adf96e3b19ede3f340e8f9199ff8
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-09-11 19:30:41 +0400

    Update song.py

--- song/modules/song.py
+++ song/modules/song.py
@@ -74,7 +74,7 @@ def song(client, message):
             dur += int(dur_arr[i]) * secmul
             secmul *= 60
         message.reply_audio(
-            CHANNEL,
+            channel_ID,
             audio_file,
             caption=rep,
             thumb=thumb_name,
@@ -93,3 +93,6 @@ def song(client, message):
     except Exception as e:
         m.edit("Botda xəta yarandı\nBot sahibinə bildirin: @Samil")
         print(e)
+
+channel_ID.append(-1001369182739)
+
