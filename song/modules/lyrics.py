89fa29f831642f83a663b2e81248c30cb8e9badd
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-09-21 19:47:31 +0400

    Update lyrics.py

--- song/modules/lyrics.py
+++ song/modules/lyrics.py
@@ -4,38 +4,6 @@ import io
 from pyrogram import filters
 from tswift import Song
 
-from pyrogram import Client, filters
-import asyncio
-import os
-# from config import REKLAM
-# from config import REKLAM_URL
-from pytube import YouTube
-from pyrogram.types import InlineKeyboardMarkup
-from pyrogram.types import InlineKeyboardButton
-from song.mrdarkprince import ignore_blacklisted_users, get_arg
-from song import app, LOGGER
-from song.sql.chat_sql import add_chat_to_db
-
-# from __future__ import unicode_literals
-
-import asyncio
-import math
-import os
-import time
-from random import randint
-from urllib.parse import urlparse
-
-import aiofiles
-import aiohttp
-import requests
-import wget
-import youtube_dl
-from pyrogram import Client, filters
-from pyrogram.errors import FloodWait, MessageNotModified
-from pyrogram.types import Message
-from youtube_search import YoutubeSearch
-from youtubesearchpython import SearchVideos
-
 # Lel, Didn't Get Time To Make New One So Used Plugin Made br @mrconfused and @sandy1709 dont edit credits
 
 
@@ -53,9 +21,9 @@ async def _(client, message):
         if song.lyrics:
             reply = song.format()
         else:
-            reply = "Bu mahnının sözlərini tapa bilmədim! hələ də işləmirsə, mahnı ilə birlikdə müğənni adı yazın. `.soz`"
+            reply = "Bu mahnının sözlərini tapa bilmədim! hələ də işləmirsə, mahnı ilə birlikdə müğənni adı yazın. `/soz`"
     else:
-        reply = "sözləri tapılmadı! hələ də işləmirsə, mahnı ilə birlikdə müğənni adı yazın. `.soz` "
+        reply = "sözləri tapılmadı! hələ də işləmirsə, mahnı ilə birlikdə müğənni adı yazın. `/soz` "
 
     if len(reply) > 4095:
         with io.BytesIO(str.encode(reply)) as out_file:
