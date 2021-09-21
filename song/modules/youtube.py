88f2280468fd7a2208bdf8a76f4ef23da61d44b6
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-09-21 20:09:58 +0400

    Update youtube.py

--- song/modules/youtube.py
+++ song/modules/youtube.py
@@ -40,6 +40,6 @@ async def ytsearch(_, message: Message):
             text += f"Uyğun mahnı adları \n\n🎵 - {results[i]['title']}\n"
             text += f"🔗 - https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
-       await m.edit(text, disable_web_page_preview=True)
+        await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
         await message.reply_text(str(e))
