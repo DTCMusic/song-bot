ba1ed1d784b89b278d791dcb5cae8d97fdad5f09
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-09-21 19:55:28 +0400

    Create youtube.py

---
+++ song/modules/youtube.py
@@ -0,0 +1,44 @@
+import logging
+
+from pyrogram.types import Message
+from youtube_search import YoutubeSearch
+from pyrogram import Client, filters
+import asyncio
+import os
+from config import REKLAM
+from config import REKLAM_URL
+from pytube import YouTube
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton
+from song.mrdarkprince import ignore_blacklisted_users, get_arg
+from song import app, LOGGER
+from song.sql.chat_sql import add_chat_to_db
+
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+import pyrogram
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+@app.on_message(pyrogram.filters.command(["axtar"]))
+async def ytsearch(_, message: Message):
+    try:
+        if len(message.command) < 2:
+            await message.reply_text("/axtar əmrinə ehtiyacı var!")
+            return
+        query = message.text.split(None, 1)[1]
+        m = await message.reply_text(f" {results[i]['title']} Axtarılır....")
+        results = YoutubeSearch(query, max_results=4).to_dict()
+        i = 0
+        text = ""
+        while i < 4:
+            text += f"Başlıq - {results[i]['title']}\n"
+            i += 1
+        await m.edit(text, disable_web_page_preview=True)
+    except Exception as e:
+        await message.reply_text(str(e))
