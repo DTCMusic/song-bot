ffd40e1610bb86280b9d380b81dc87ca58f875e1
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-09-21 19:37:02 +0400

    Update lyrics.py

--- song/modules/lyrics.py
+++ song/modules/lyrics.py
@@ -1,8 +1,8 @@
 import io
 
-from pyrogram import Client as pbot
+# from pyrogram import Client as pbot
 from pyrogram import filters
-from tswift import Song
+# from tswift import Song
 
 from pyrogram import Client, filters
 import asyncio
