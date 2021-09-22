307ff02989e69a7f83ea48fbcad0bd42c3105540
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-09-22 11:51:07 +0400

    Update chats.py

--- song/modules/chats.py
+++ song/modules/chats.py
@@ -43,7 +43,7 @@ async def list(client, message):
 #        songs.append(msg.message_id)
 #     songsnum = len(songs)
     del chats, users # , songs
-    message.reply(
+    await message.reply_text(
         reply_to_message_id=message.message_id,
         chat_id=message.chat.id,
         text=f"**Bot statistikası\n\nQrup sayı:** `{chatsnum}`\n**İstifadəçi sayı:** `{usersnum}`", # \n**Yüklənən mahnılar:** `{songsnum}`",
