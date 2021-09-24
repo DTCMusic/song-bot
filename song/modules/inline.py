65ca985f17b659a680d144cee3ad11f020209278
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-09-24 16:51:26 +0400

    Create inline.py

---
+++ song/modules/inline.py
@@ -0,0 +1,82 @@
+from pyrogram import Client, errors
+from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+
+from youtubesearchpython import VideosSearch
+
+from pyrogram import Client, filters
+import asyncio
+import os
+from config import REKLAM
+from config import REKLAM_URL
+from pytube import YouTube
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton
+from song.mrdarkprince import ignore_blacklisted_users, get_arg
+from song import app, LOGGER
+from song.sql.chat_sql import add_chat_to_db
+
+# from __future__ import unicode_literals
+
+import asyncio
+import math
+import time
+from random import randint
+from urllib.parse import urlparse
+
+import aiofiles
+import aiohttp
+import requests
+import wget
+import youtube_dl
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait, MessageNotModified
+from pyrogram.types import Message
+from youtube_search import YoutubeSearch
+from youtubesearchpython import SearchVideos
+
+
+@app.on_inline_query()
+async def inline(client: Client, query: InlineQuery):
+    answers = []
+    search_query = query.query.lower().strip().rstrip()
+
+    if search_query == "":
+        await client.answer_inline_query(
+            query.id,
+            results=answers,
+            switch_pm_text="Youtube-də axtar",
+            switch_pm_parameter="start",
+            cache_time=0
+        )
+    else:
+        search = VideosSearch(search_query, limit=50)
+
+        for result in search.result()["result"]:
+            answers.append(
+                InlineQueryResultArticle(
+                    title=result["title"],
+                    description="{}, {} views.".format(
+                        result["duration"],
+                        result["viewCount"]["short"]
+                    ),
+                    input_message_content=InputTextMessageContent(
+                        "https://www.youtube.com/watch?v={}".format(
+                            result["id"]
+                        )
+                    ),
+                    thumb_url=result["thumbnails"][0]["url"]
+                )
+            )
+
+        try:
+            await query.answer(
+                results=answers,
+                cache_time=0
+            )
+        except errors.QueryIdInvalid:
+            await query.answer(
+                results=answers,
+                cache_time=0,
+                switch_pm_text="Axtarış vaxtı bitdi",
+                switch_pm_parameter="",
+            )
