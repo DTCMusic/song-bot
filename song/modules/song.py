5b0edb7b03ce859ae2a3a15fb3ddd526184ce09a
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-10-04 16:03:05 +0400

    Update song.py

--- song/modules/song.py
+++ song/modules/song.py
@@ -278,51 +278,136 @@ def time_to_seconds(times):
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
 
 
-@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("vsong"))
-async def vsong(client, message):
-    ydl_opts = {
-        "format": "best",
-        "keepvideo": True,
-        "prefer_ffmpeg": False,
-        "geo_bypass": True,
-        "outtmpl": "%(title)s.%(ext)s",
-        "quite": True,
-    }
-    query = " ".join(message.command[1:])
-    try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
-        title = results[0]["title"][:40]
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"thumb{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-        results[0]["duration"]
-        results[0]["url_suffix"]
-        results[0]["views"]
-        message.from_user.mention
-    except Exception as e:
-        print(e)
-    try:
-        msg = await message.reply(f"📥 {title} **video yüklənir...**")
-        with YoutubeDL(ydl_opts) as ytdl:
-            ytdl_data = ytdl.extract_info(link, download=True)
-            file_name = ytdl.prepare_filename(ytdl_data)
-    except Exception as e:
-        return await msg.edit(f"🚫 **Xəta:** {e}")
-    preview = wget.download(thumbnail)
-#     await msg.edit("📤 **Video yüklənir...**")
-    await message.reply_video(
-        file_name,
-        duration=int(ytdl_data["duration"]),
-        thumb=preview,
-        caption=ytdl_data["title"],
-        reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(f"📽 Video Play", url=f"t.me/videoazz")
-                        ]
-                    ]
-                ),
-        )
+# @app.on_message(filters.create(ignore_blacklisted_users) & filters.command("vsong"))
+# async def vsong(client, message):
+#     ydl_opts = {
+#         "format": "best",
+#         "keepvideo": True,
+#         "prefer_ffmpeg": False,
+#         "geo_bypass": True,
+#         "outtmpl": "%(title)s.%(ext)s",
+#         "quite": True,
+#     }
+#     query = " ".join(message.command[1:])
+#     try:
+#         results = YoutubeSearch(query, max_results=1).to_dict()
+#         link = f"https://youtube.com{results[0]['url_suffix']}"
+#         title = results[0]["title"][:40]
+#         thumbnail = results[0]["thumbnails"][0]
+#         thumb_name = f"thumb{title}.jpg"
+#         thumb = requests.get(thumbnail, allow_redirects=True)
+#         open(thumb_name, "wb").write(thumb.content)
+#         results[0]["duration"]
+#         results[0]["url_suffix"]
+#         results[0]["views"]
+#         message.from_user.mention
+#     except Exception as e:
+#         print(e)
+#     try:
+#         msg = await message.reply(f"📥 {title} **video yüklənir...**")
+#         with YoutubeDL(ydl_opts) as ytdl:
+#             ytdl_data = ytdl.extract_info(link, download=True)
+#             file_name = ytdl.prepare_filename(ytdl_data)
+#     except Exception as e:
+#         return await msg.edit(f"🚫 **Xəta:** {e}")
+#     preview = wget.download(thumbnail)
+# #     await msg.edit("📤 **Video yüklənir...**")
+#     await message.reply_video(
+#         file_name,
+#         duration=int(ytdl_data["duration"]),
+#         thumb=preview,
+#         caption=ytdl_data["title"],
+#         reply_markup=InlineKeyboardMarkup(
+#                     [
+#                         [
+#                             InlineKeyboardButton(f"📽 Video Play", url=f"t.me/videoazz")
+#                         ]
+#                     ]
+#                 ),
+#         )
 
+
+@app.on_message(filters.command(["vsong", "video"]))
+async def ytmusic(client, message: Message):
+    global is_downloading
+    if is_downloading:
+        await message.reply_text(
+            "Another download is in progress, try again after sometime."
+        )
+        return
+
+    urlissed = get_text(message)
+
+    pablo = await client.send_message(
+        message.chat.id, f"`Getting {urlissed} From Youtube Servers. Please Wait.`"
+    )
+    if not urlissed:
+        await pablo.edit("Invalid Command Syntax, Please Check Help Menu To Know More!")
+        return
+
+    search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi["search_result"]
+    mo = mio[0]["link"]
+    thum = mio[0]["title"]
+    fridayz = mio[0]["id"]
+    thums = mio[0]["channel"]
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    url = mo
+    sedlyf = wget.download(kekme)
+    opts = {
+        "format": "best",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
+        "outtmpl": "%(id)s.mp4",
+        "logtostderr": False,
+        "quiet": True,
+    }
+    try:
+        is_downloading = True
+        with youtube_dl.YoutubeDL(opts) as ytdl:
+            infoo = ytdl.extract_info(url, False)
+            duration = round(infoo["duration"] / 60)
+
+            if duration > DURATION_LIMIT:
+                await pablo.edit(
+                    f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+                )
+                is_downloading = False
+                return
+            ytdl_data = ytdl.extract_info(url, download=True)
+
+    except Exception:
+        # await pablo.edit(event, f"**Failed To Download** \n**Error :** `{str(e)}`")
+        is_downloading = False
+        return
+
+    c_time = time.time()
+    file_stark = f"{ytdl_data['id']}.mp4"
+    capy = f"**Video Name ➠** `{thum}` \n**Requested For :** `{urlissed}` \n**Channel :** `{thums}` \n**Link :** `{mo}`"
+    await client.send_video(
+        message.chat.id,
+        video=open(file_stark, "rb"),
+        duration=int(ytdl_data["duration"]),
+        file_name=str(ytdl_data["title"]),
+        thumb=sedlyf,
+        caption=capy,
+        supports_streaming=True,
+        progress=progress,
+        progress_args=(
+            pablo,
+            c_time,
+            f"`Uploading {urlissed} Song From YouTube Music!`",
+            file_stark,
+        ),
+    )
+    await pablo.delete()
+    is_downloading = False
+    for files in (sedlyf, file_stark):
+        if files and os.path.exists(files):
+            os.remove(files)
