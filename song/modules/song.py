2e5f7690dfb960b5a9d452db7520449f5c85a3d0
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-10-20 09:22:11 +0400

    Update song.py

--- song/modules/song.py
+++ song/modules/song.py
@@ -98,7 +98,7 @@ def a(client, message):
         caption=rep,quote=False, 
         title=title, 
         duration=dur, 
-        performer=f"{name} tərəfindən", #str(info_dict["uploader"]), 
+        performer= str(info_dict["uploader"]), 
         thumb=thumb_name,
         reply_markup=InlineKeyboardMarkup(
                     [
