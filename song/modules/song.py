d9678957788d2b405cceba895aad4b79b530fb39
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-11-22 11:53:15 +0400

    Update song.py

--- song/modules/song.py
+++ song/modules/song.py
@@ -1,89 +1,76 @@
-from __future__ import unicode_literals
+from pyrogram import Client, filters
 
-import asyncio
-import math
+import youtube_dl
+from youtube_search import YoutubeSearch
+import requests
+
 import os
 import time
-from random import randint
-from urllib.parse import urlparse
-
-import aiofiles
-import aiohttp
-import requests
-import wget
-import yt_dlp
-from pyrogram import Client, filters
-from config import ARQ_API_KEY
-from pyrogram.errors import FloodWait, MessageNotModified
-from pyrogram.types import Message
-from youtube_search import YoutubeSearch
-from youtubesearchpython import SearchVideos
+from config import Config
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from song import app, LOGGER
 
-# aiohttpsession = aiohttp.ClientSession()
-# chat_id = None
-# arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
-# DISABLED_GROUPS = []
-# useer = "NaN"
-
-DURATION_LIMIT = 10
-
-@app.on_message(filters.command("song") & ~filters.channel)
-def song(client, message):
-
-    user_id = message.from_user.id
-    user_name = message.from_user.first_name
-    rpk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
-
-    query = ""
+app.on_message(filters.command(['song']))
+def a(client, message):
+    query = ''
     for i in message.command[1:]:
-        query += " " + str(i)
+        query += ' ' + str(i)
     print(query)
-    m = message.reply("🔎 Mahnı axtarılır...")
+    m = message.reply('🔎 Axtarılır')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
-        # print(results)
-        title = results[0]["title"][:40]
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"thumb{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count>0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+            views = results[0]["views"]
 
-        duration = results[0]["duration"]
-        results[0]["url_suffix"]
-        results[0]["views"]
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 7000:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
 
+            performer = f"[@Songazbot]" 
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+
+        except Exception as e:
+            print(e)
+            m.edit('**Mahnı tapılmadı yenidən yoxlayın !**')
+            return
     except Exception as e:
-        m.edit("Mahnı adını düzgün yazın")
+        m.edit(
+            "**/song əmri ilə mahnı adını yazın**"
+        )
         print(str(e))
         return
-    m.edit(f"{title} Yüklənir ✅")
+    m.edit("`Yüklənir`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"🎵 `{title}`"
-        secmul, dur, dur_arr = 1, 0, duration.split(":")
-        for i in range(len(dur_arr) - 1, -1, -1):
-            dur += int(dur_arr[i]) * secmul
+        rep = f'🎶 {title}'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        message.reply_audio(
-            audio_file,
-            caption=rep,
-            thumb=thumb_name,
-            parse_mode="md",
-            title=title,
-            performer="@Songazbot",
-            duration=dur,
-        )
+        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name)
         m.delete()
     except Exception as e:
-        m.edit("❌ Error")
+        m.edit('**Xəta baş verdi bot sahibi ilə əlaqəyə keç @Samil**')
         print(e)
-
     try:
         os.remove(audio_file)
         os.remove(thumb_name)
