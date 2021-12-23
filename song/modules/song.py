21a7a08d222f3f801060ef93516c342962b85b2f
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-12-23 23:19:01 +0400

    Update song.py

--- song/modules/song.py
+++ song/modules/song.py
@@ -32,7 +32,12 @@ from song.modules.database import Database
 async def _(bot, cmd):
     await handle_user_status(bot, cmd)
 
-# @app.on_message(filters.group) #& filters.group
+@app.on_message(filters.private)
+async def _(bot, cmd): 
+    await handle_user_status(bot, cmd)
+
+
+# @app.on_message(filters.ytregex) #& filters.group
 # async def _(bot, cmd):
 #     await handle_user_status(bot, cmd)
     
@@ -44,6 +49,8 @@ ydl_opts = {
         'outtmpl':'%(title)s.%(ext)s',
         'quite':True
 }
+
+ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
 
 
 @app.on_message(filters.command("song"))
@@ -109,6 +116,73 @@ def song(bot, cmd): #client, message,
         os.remove(thumb_name)
     except Exception as e:
         print(e)
+
+
+
+@app.on_message(filters.regex(ytregex))
+def song(bot, cmd): #client, message,
+    query = " ".join(cmd.command[1:])
+    m = cmd.reply("🔎 Mahnı axtarılır...")
+    ydl_ops = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
+        name = cmd.from_user["first_name"]
+
+    except Exception as e:
+        m.edit("Zəhmət olmasa mahnı adını düzgün yazın!")
+        print(str(e))
+        return
+    m.edit("📥 Mahnı yüklənir...")
+    try:
+        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f"🎵 {title}"
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(float(dur_arr[i])) * secmul
+            secmul *= 60
+        m.edit(f"📤 `{title}`")
+        mess = cmd.reply_audio(
+            audio_file,
+            caption=rep,
+            thumb=thumb_name,
+            performer="Song 🇦🇿",
+            parse_mode="md",
+            title=title,
+            duration=dur,
+            reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton(f"🎵 Play List", url=f"t.me/songazz")
+                        ]
+                    ]
+                ),
+        )
+        bot.copy_message(
+            -1001512529266,
+            cmd.chat.id,
+            mess.message_id
+        )
+        m.delete()
+    except Exception as e:
+        m.edit("😊 Bizi seçdiyiniz üçün təşəkkürlər\n Hər hansı Prablem olarsa @Samil - ə bildirin")
+        print(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
+
 
 
 
