c1d3fb0d8df8b6bd0bb8d8afb6b537bd581b94f4
Shamil <76031213+Texnocom@users.noreply.github.com>
2021-12-24 00:08:16 +0400

    Update song.py

--- song/modules/song.py
+++ song/modules/song.py
@@ -79,7 +79,7 @@ def song(bot, cmd): #client, message,
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"🎵 {title}"
+        rep = f"@AlhayaatJamila 🎵"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
@@ -89,17 +89,10 @@ def song(bot, cmd): #client, message,
             audio_file,
             caption=rep,
             thumb=thumb_name,
-            performer="Song 🇦🇿",
+            performer="ᴀʟʜᴀʏᴀᴀᴛ ᴊᴀᴍɪʟᴀོ",
             parse_mode="md",
             title=title,
-            duration=dur,
-            reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(f"🎵 Play List", url=f"t.me/songazz")
-                        ]
-                    ]
-                ),
+            duration=dur
         )
         bot.copy_message(
             -1001512529266,
