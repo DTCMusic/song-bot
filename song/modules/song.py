c38ffc54c3f7034f35655d6aa066866edfc43b74
Shamil <76031213+Texnocom@users.noreply.github.com>
2022-03-10 12:25:11 +0400

    Update song.py

--- song/modules/song.py
+++ song/modules/song.py
@@ -62,7 +62,7 @@ def song(bot, cmd): #client, message,
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"🎵 `{title}`"
+        rep = f"🎵 `{title}`\n🎶 Play List: @Songazz"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
@@ -72,7 +72,7 @@ def song(bot, cmd): #client, message,
             audio_file,
             caption=rep,
             thumb=thumb_name,
-            performer="Ukrayna 🇺🇦/🇦🇿",
+            performer="@Songazbot",
             parse_mode="md",
             title=title,
             duration=dur,
