85c47336f9441fb811fe8065767b0aa8b86ac840
Shamil <76031213+Texnocom@users.noreply.github.com>
2022-04-30 15:59:00 +0400

    Update __main__.py

--- song/__main__.py
+++ song/__main__.py
@@ -5,6 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 from song import app, LOGGER
 from song.mrdarkprince import ignore_blacklisted_users
 from song.sql.chat_sql import add_chat_to_db
+from config import OWNER_ID
 
 import aiohttp
 import requests
