6a709f0125a92790a1077ed23fbef909891316e0
Shamil <76031213+Texnocom@users.noreply.github.com>
2022-05-09 09:30:21 +0400

    Update song.py

--- song/modules/song.py
+++ song/modules/song.py
@@ -75,7 +75,7 @@ def song(bot, cmd): #client, message,
             title=title,
             duration=dur
         )
-         bot.copy_message(
+        await cmd.copy_message(
              -1001512529266,
              cmd.chat.id,
              mess.message_id
